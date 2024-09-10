289127ba8fdd5cc2dfabae60b3eb1bae781cd153
IsabelliAurelio <isabelliaurelioportfolio@gmail.com>
2024-09-10 10:38:29 -0300

    Add files via upload

---
+++ 04-Nome-idade.py
@@ -0,0 +1,15 @@
+# Atribuição de variáriaveis
+Nome = "Isabelli"
+Idade = 16
+Altura = 1.60
+
+# Exibir mensagem na tela
+print("Nome", Nome, "Idade", idade, "altura", altura)
+# Formação com f-string
+print("Nome: {Nome} | Idade: {Idade} | Altura: {Altura}")
+
+Nome = imput("Digite o seu nome: ")
+Idade = int(input("Digite a sua idade: "))
+altura = float(input("Digite a sua altura: "))
+
+print("Nome: {nome} | Idade: {idade} | Altura: {altura}")
